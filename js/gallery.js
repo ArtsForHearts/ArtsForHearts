4b24cb0a0d14fb0d804902c5f30d13a195f7a932
Rich Ramsland <rramsland@gmail.com>
2017-03-20 04:35:31 -0400

    hid ajax request

--- js/gallery.js
+++ js/gallery.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
-	var folder = "../images/GalleryPhotos2016/";
+	//var folder = "../images/GalleryPhotos2016/";
 
-					$.ajax({
-					    url : folder,
-					    success: function (data) {
-					        $(data).find("a").attr("href", function (i, val) {
-					            if( val.match(/\.(jpe?g|png|gif|JPG)$/) ) { 
-					                //$("#gallery").append( "<img src='"+ folder + val +"'>" );
-					                $("#gallery").append( "<a href='"+ folder + val +"'>" + "<img src=''>" + "</a>" );
-					            } 
-					        });
-					    }
-					});
+					//$.ajax({
+					//    url : folder,
+					//    success: function (data) {
+					//        $(data).find("a").attr("href", function (i, val) {
+					//            if( val.match(/\.(jpe?g|png|gif|JPG)$/) ) { 
+					//                //$("#gallery").append( "<img src='"+ folder + val +"'>" );
+					//                $("#gallery").append( "<a href='"+ folder + val +"'>" + "<img src=''>" + "</a>" );
+					//            } 
+					//        });
+					//    }
+					//});
 					$('#gallery').photobox('a',{ time:0 });
 
 });
